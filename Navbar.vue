f54f77e95ebdf926287c2d836b5f3dfa41fb4ca1
ahmdRmaa <158021611+ahmdRmaa@users.noreply.github.com>
2024-12-30 12:03:22 +0700

    Add files via upload

---
+++ Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav class="bg-blue-500 text-white p-4 flex justify-between">
+        <h1>Web Smkn4</h1>
+        <ul class="flex">
+            <li class="mr-4"><Nuxtlink to="/">Beranda</Nuxtlink></li>
+            <li class="mr-4"><Nuxtlink to="/">Tentang</Nuxtlink></li>
+            <li class="mr-4"><Nuxtlink to="Berita">Berita</Nuxtlink></li>
+        </ul>
+    </nav>
+
+</template>
---
+++ index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Navbar />
+        <div class="container mx-auto p-4">
+            <h1 class="text-3x1 font-bold mb-4">Selamat datang di website SMK NEGERI 4 TASIKMALAYA
+                <p>Ini halaman utama website smkn4
+                    </div>
+                    <Footer />
+    </div>
+</template>
+<script>
+export default {
+    components: { Navbar, Footer },
+}
+</script>
